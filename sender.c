940aa103bfd12196c265c3d0ded3a4370efbb791
prateekgargx <84305275+prateekgargX@users.noreply.github.com>
2022-10-11 23:00:58 +0530

    Create files
    driver code for sender and receiver

---
+++ sender.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX_LINE 256
+
+int main(int argc, char * argv[])
+{
+ 
+}
