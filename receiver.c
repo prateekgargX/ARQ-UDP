2d46dc8ee89760f81f68410314bc0a54dc4726f9
prateekgargx <84305275+prateekgargX@users.noreply.github.com>
2022-10-12 13:49:27 +0530

    Complete minimal working code
    To be done:
    1. Add more commets
    2. clean up the code and optimise on some branches.
    
    Big Stuff:
    Currently capable of only handling 8 packets
    README needs attention as well

--- receiver.c
+++ receiver.c
@@ -1,18 +1,91 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
 
-#define MAX_LINE 256
+#define MAXLINE 1024
 
 int main(int argc, char * argv[])
 {
-    int senderPort = atoi(argv[1]);
-    int receiverPort = atoi(argv[2]);
-    int transmissionTime = atof(argv[3]);
-    int numPackets =  atoi(argv[4]);
+	// if(argc <4){
+	// 	perror("Incomplete Arguments");
+	// 	exit(EXIT_FAILURE);
+	// }
+	
+    int receiverPort = atoi(argv[1]);
+    int senderPort = atoi(argv[2]);  //Not used
+    double packetDropProbability = atof(argv[3]);
+
+	int socketDesc;
+	char buffer[MAXLINE];
+	struct sockaddr_in servaddr, cliaddr;
+	double randNum;
+	srand ( time ( NULL));	
+
+	// Creating socket file descriptor
+	if ( (socketDesc = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+		
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(receiverPort);
+		
+	// Bind the socket with the server address
+	if ( bind(socketDesc, (const struct sockaddr *)&servaddr,sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	int len, n;
+	int expectedID = 1;
+	for (;;)
+	{		
+	
+		len = sizeof(cliaddr); //len is value/result
+		
+		n = recvfrom(socketDesc, (char *)buffer, MAXLINE,
+					MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+					&len);
+		buffer[n] = '\0';
+		int packetID = (buffer[n-1] - '0');
+
+
+		//printf("Received %s\n", buffer);
+
+		if(packetID==expectedID){
+
+			randNum = (double)rand()/RAND_MAX; 
+			if(randNum>=packetDropProbability){
+				expectedID++;
+				char ACK[]="Acknowledgement:0";
+				ACK[16] = expectedID+'0';
+
+				sendto(socketDesc, (const char *)ACK, strlen(ACK),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
+				printf("Sent %s\n",ACK);
+			} else printf("Dropped %s\n",buffer); 
+			
+		} else {
+			char ACK[]="Acknowledgement:0";
+			ACK[16] = expectedID+'0';
+
+			sendto(socketDesc, (const char *)ACK, strlen(ACK),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
+			printf("Sent %s\n",ACK);
+		}
+	}
+
+	return 0;
+
+
 }
--- sender.c
+++ sender.c
@@ -1,17 +1,77 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-#define MAX_LINE 256
+#define MAXLINE 1024
 
 int main(int argc, char * argv[])
 {
-    int senderPort = atoi(argv[2]);
-    int receiverPort = atoi(argv[1]);
-    int packetDropProbability = atof(argv[3]);
+    // if(argc <5){
+	// 	perror("Incomplete Arguments");
+	// 	exit(EXIT_FAILURE);
+	// }
+	
+    int senderPort = atoi(argv[1]);
+    int receiverPort = atoi(argv[2]); //Not used
+    int retransmissionTime = atof(argv[3]);
+    int numPackets =  atoi(argv[4]);
+
+
+	int socketDesc;
+	char buffer[MAXLINE]; 
+	struct sockaddr_in servaddr;
+	
+	struct timeval tv;
+	tv.tv_sec = retransmissionTime/1000;
+	tv.tv_usec = 0;
+
+	// Creating socket file descriptor
+	if ( (socketDesc = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if ( setsockopt(socketDesc, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0 ){
+		perror("setsockopt failed");
+		exit(EXIT_FAILURE);
+		}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(buffer,0,MAXLINE);
+
+	// Filling server information
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(senderPort);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+		
+	int n, len;	
+	for (int i = 1; i <= numPackets; i++)
+	{
+		///assuming 0<i+1<9 
+	    char Packet[]="Packet:0";
+	    Packet[7] = '0'+i; 
+
+		sendto(socketDesc, (const char *)Packet, strlen(Packet),MSG_CONFIRM, (const struct sockaddr *) &servaddr,sizeof(servaddr));
+		printf("Sent Packet:%d\n",i);
+				
+		n = recvfrom(socketDesc, (char *)buffer, MAXLINE,0, (struct sockaddr *) &servaddr,&len);
+		buffer[n] = '\0';
+		int ackID = (buffer[16]-'0');
+
+		if(n < 0){i--; printf("Retransmission Timer Expired\n"); continue;}
+		//kprintf("Received %s\n", buffer);
+
+		if(i+1!=ackID){i--; continue;}
+
+
+	}
+
+	close(socketDesc);
+	return 0;
+
 }
